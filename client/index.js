0490362b0ead869668d24d31236177470caed97b
Greg Kovshov <barkev2009@bk.ru>
2024-04-03 22:36:16 +0700

    minor fix

--- client/index.js
+++ client/index.js
@@ -7,8 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  console.log('Wedding App is deployed');
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Wedding App Client started on port ${PORT}`));
