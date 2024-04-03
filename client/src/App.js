ce4e7d69a433b784488a99428405058bea5a7d2c
Greg Kovshov <barkev2009@bk.ru>
2024-04-03 22:58:23 +0700

    restricted desktop

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import style from './style.module.css';
 function App() {
   return (
     <div>
+      <div className={style.pc_blocker}>
+        Мы пока не сделали дизайн для десктопа, поэтому просим перейти на мобильную версию) 🤷
+      </div>
       WEDDING HERE!
     </div>
   );
