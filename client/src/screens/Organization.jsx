ee91a964c6611b76df7ee576466359a352756e7c
Greg Kovshov <barkev2009@bk.ru>
2024-04-07 19:20:43 +0700

    whatsapp text

--- client/src/screens/Organization.jsx
+++ client/src/screens/Organization.jsx
@@ -4,21 +4,25 @@ import { GENDERS } from '../const';
 
 const Organization = ({ link }) => {
 
-    let pay, ensure;
+    let pay, ensure, writing;
     switch (link.gender) {
         case GENDERS.male.code:
             pay = 'Обрати';
             ensure = ' позаботься';
+            writing = 'пишет';
             break;
         case GENDERS.female.code:
             pay = 'Обрати';
             ensure = ' позаботься';
+            writing = 'пишет';
             break;
         default:
             pay = 'Обратите';
             ensure = ' позаботьтесь';
+            writing = 'пишут';
             break;
     }
+    const generatedText = `Здравствуйте, Алена! Вам ${writing} ${link.name}, мы были гостями на свадьбе Ани и Гриши.`.split(' ').join('%20');
 
     return (
         <div className={[styles.screen].join(' ')}>
@@ -29,7 +33,7 @@ const Organization = ({ link }) => {
                 организатор Алёна: </h3>
             <h3 className={[styles.green_font, styles.center_text].join(' ')}>
                 <a href="tel:+79537672033">+7 (953) 767-20-33</a></h3>
-            <a aria-label="Chat on WhatsApp" href="https://wa.me/+79537672033?text=Здравствуйте,%20Алена!">
+            <a aria-label="Chat on WhatsApp" href={`https://wa.me/+79537672033?text=${generatedText}`}>
                 <div>WHATSAPP</div>
             </a>
             <h3 className={[styles.green_font, styles.center_text].join(' ')}>
