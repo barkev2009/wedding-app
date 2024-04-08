03b71606d3660dcbc3cf8633a752dd8dee84fc46
Greg Kovshov <barkev2009@bk.ru>
2024-04-08 21:12:31 +0700

    styling for 404

--- client/src/routes/NoData.jsx
+++ client/src/routes/NoData.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styles from '../screens/screen.module.css';
 
 const NoData = () => {
   return (
-    <div>NoData</div>
+    <div className={[styles.green, styles.screen].join(' ')}>
+      <h1 className={[styles.white_font, styles.center_text].join(' ')}>
+      <span style={{fontSize: '7rem'}}>404</span> <br />
+      Упс...<br />
+      Кажется, что эта ссылка никуда не ведет, попробуйте другую)
+      </h1>
+    </div>
   )
 }
 
