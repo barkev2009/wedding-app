7f2c301fd11659f9d2aeefbc7618efbd88b169d0
Greg Kovshov <barkev2009@bk.ru>
2024-04-09 00:31:45 +0700

    styling for time-place

--- client/src/screens/TimePlace.jsx
+++ client/src/screens/TimePlace.jsx
@@ -3,17 +3,19 @@ import styles from './screen.module.css';
 
 const TimePlace = () => {
     return (
-        <div className={[styles.screen, styles.green].join(' ')}>
+        <div className={[styles.screen, styles.green, styles.flex_toggle_direction, styles.flex_center].join(' ')}>
             <h1 className={[styles.white_font, styles.center_text].join(' ')}>Место и дата <br /> проведения</h1>
-            <h2 className={[styles.white_font, styles.center_text].join(' ')}>18.07.2024</h2>
-            <h2 className={[styles.white_font, styles.center_text].join(' ')}>
-                Ресторан <br />
-                Paris в Double Tree <br />
-                by Hilton Hotel
-            </h2>
-            <h3 className={[styles.white_font, styles.center_text].join(' ')}>
-                г. Новосибирск, ул. Каменская, 7/1
-            </h3>
+            <div>
+                <h2 className={[styles.white_font, styles.center_text, styles.date].join(' ')}>18.07.2024</h2>
+                <h2 className={[styles.white_font, styles.center_text, styles.restaurant].join(' ')}>
+                    Ресторан <br />
+                    Paris в Double Tree <br />
+                    by Hilton Hotel
+                </h2>
+                <h3 className={[styles.white_font, styles.center_text].join(' ')}>
+                    г. Новосибирск, ул. Каменская, 7/1
+                </h3>
+            </div>
         </div>
     )
 }
