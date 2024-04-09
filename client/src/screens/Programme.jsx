d1e984f2d9d443fb4d8ab3166496dcc46c2c805d
Greg Kovshov <barkev2009@bk.ru>
2024-04-09 18:16:32 +0700

    styling for programme

--- client/src/screens/Programme.jsx
+++ client/src/screens/Programme.jsx
@@ -3,21 +3,22 @@ import styles from './screen.module.css';
 
 const Programme = () => {
     return (
-        <div className={[styles.screen, styles.green].join(' ')}>
+        <div className={[styles.screen, styles.green, styles.programme].join(' ')}>
             <h1 className={[styles.white_font, styles.center_text].join(' ')}>Программа</h1>
             <div>
-                <h2 className={[styles.white_font, styles.center_text].join(' ')}>16:30</h2>
-                <h3 className={[styles.white_font, styles.center_text].join(' ')}>сбор гостей</h3>
+                <div className={styles.programmeContainer}>
+                    <h2 className={[styles.white_font, styles.center_text].join(' ')}>16:30</h2>
+                    <h3 className={[styles.white_font, styles.center_text].join(' ')}>сбор гостей</h3>
+                </div>
+                <div className={styles.programmeContainer}>
+                    <h2 className={[styles.white_font, styles.center_text].join(' ')}>17:00</h2>
+                    <h3 className={[styles.white_font, styles.center_text].join(' ')}>банкет</h3>
+                </div>
+                <div className={styles.programmeContainer}>
+                    <h2 className={[styles.white_font, styles.center_text].join(' ')}>22:00</h2>
+                    <h3 className={[styles.white_font, styles.center_text].join(' ')}>дискотека</h3>
+                </div>
             </div>
-            <div>
-                <h2 className={[styles.white_font, styles.center_text].join(' ')}>17:00</h2>
-                <h3 className={[styles.white_font, styles.center_text].join(' ')}>банкет</h3>
-            </div>
-            <div>
-                <h2 className={[styles.white_font, styles.center_text].join(' ')}>22:00</h2>
-                <h3 className={[styles.white_font, styles.center_text].join(' ')}>дискотека</h3>
-            </div>
-
         </div>
     )
 }
