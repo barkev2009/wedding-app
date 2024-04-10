70d042ecb5771164da2b4304dc93dc223b5e8176
Greg Kovshov <barkev2009@bk.ru>
2024-04-10 22:15:08 +0700

    styling for organization

--- client/src/screens/Organization.jsx
+++ client/src/screens/Organization.jsx
@@ -32,24 +32,29 @@ const Organization = ({ link }) => {
     const generatedText = `Здравствуйте, Алена! Вам ${writing} ${link.name}, ${who} - ${guest} на свадьбе Ани и Гриши.`.split(' ').join('%20');
 
     return (
-        <div className={[styles.screen, styles.flex_center].join(' ')}>
+        <div className={[styles.screen, styles.flex_center, styles.organization].join(' ')}>
             <h1 className={[styles.green_font, styles.center_text].join(' ')}>Организация</h1>
-            <h3 className={[styles.green_font, styles.center_text].join(' ')}>
-                На все вопросы связанные с
-                торжеством, с радостью ответит наш
-                организатор Алёна: </h3>
-            <h3 className={[styles.green_font, styles.center_text].join(' ')}>
-                <a href="tel:+79537672033">+7 (953) 767-20-33</a></h3>
-            <a aria-label="Chat on WhatsApp" href={`https://wa.me/+79537672033?text=${generatedText}`}>
-                <WhatsApp />
-            </a>
-            <h3 className={[styles.green_font, styles.center_text].join(' ')}>
-                {pay} внимание, что место и
-                формат мероприятия не
-                предполагают детской площадки и
-                аниматоров. Пожалуйста,
-                {ensure} о том, чтобы провести
-                этот вечер без детей</h3>
+            <div className={[styles.center_text, styles.flex_center].join(' ')}>
+                <div className={[styles.green_font, styles.center_text].join(' ')}>
+                    <h3>На все вопросы связанные с
+                        торжеством, с радостью ответит наш
+                        организатор Алёна:</h3>
+                    <h3 className={[styles.green_font, styles.center_text].join(' ')}>
+                        <a className={[styles.green_font, styles.center_text].join(' ')} href="tel:+79537672033">+7 (953) 767-20-33</a>
+                    </h3>
+                </div>
+                <a className={styles.whatsapp} aria-label="Chat on WhatsApp" href={`https://wa.me/+79537672033?text=${generatedText}`}>
+                    <WhatsApp />
+                </a>
+                <h3 className={[styles.green_font, styles.center_text].join(' ')}>
+                    {pay} внимание, что место и
+                    формат мероприятия не
+                    предполагают детской площадки и
+                    аниматоров. Пожалуйста,
+                    {ensure} о том, чтобы провести
+                    этот вечер без детей
+                </h3>
+            </div>
         </div>
     )
 }
