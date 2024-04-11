c1447c0ac251a38a4481462c8678a7817ed841d7
Greg Kovshov <barkev2009@bk.ru>
2024-04-11 15:50:16 +0700

    static

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const errorHandler = require('./middleware/ErrorHandlerMiddleware');
 const models = require('./models/models');
 const http = require('http');
 const router = require('./routers/index');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 const app = express()
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
 app.use('/api', router);
 
 // Middleware с ошибками должен регистрироваться в последнюю очередь!!!
