25a20c71054b866d90782828e7252510a48df015
Greg Kovshov <barkev2009@bk.ru>
2024-04-12 10:36:15 +0700

    viewport height

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import AppRouter from './routes/AppRouter';
 import { injectStores } from '@mobx-devtools/tools';
 import user from './store/user';
 import './style.css';
-import {YMInitializer} from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 
 injectStores(
   { user }
@@ -13,16 +13,22 @@ injectStores(
 
 const App = observer(
   () => {
+    const setHeight = () => {
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+    }
+    setHeight();
+    window.addEventListener('resize', () => { setHeight()})
 
     user.checkAuth();
 
     return (
       <>
-      <YMInitializer accounts={[96989883]} options={{clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true}} />
-      <BrowserRouter>
-        <Blocker />
-        <AppRouter />
-      </BrowserRouter>
+        <YMInitializer accounts={[96989883]} options={{ clickmap: true, trackLinks: true, accurateTrackBounce: true, webvisor: true }} />
+        <BrowserRouter>
+          <Blocker />
+          <AppRouter />
+        </BrowserRouter>
       </>
     );
   }
