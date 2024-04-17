a960d51d28caaa12014d8b51c4145cf5807821ab
Greg Kovshov <barkev2009@bk.ru>
2024-04-17 23:16:40 +0700

    add interaction to glass

--- client/src/screens/Details.jsx
+++ client/src/screens/Details.jsx
@@ -1,14 +1,72 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './screen.module.css';
 import { GENDERS } from '../const';
 import png from '../images/envelope.png';
 import glass from '../images/glass.png';
-import { useWineIntersectionObserver } from '../hooks';
 import ym from 'react-yandex-metrika';
 
 const Details = ({ link }) => {
 
-    useWineIntersectionObserver();
+    const timeouts = useRef([]);
+
+    let options = {
+        root: document.querySelector(styles.details),
+        rootMargin: "0px",
+        threshold: 1.0,
+    };
+
+    const callback = (entries) => {
+        entries.forEach(entry => {
+            const wine = document.querySelector('.' + styles.wine);
+            let timeout;
+            if (entry.isIntersecting) {
+                for (let i = 10; i < 43; i++) {
+                    timeout = setTimeout(
+                        () => {
+                            wine.style.height = `${i}px`;
+                        }, (i - 10) * 100
+                    )
+                    timeouts.current.push(timeout);
+                }
+            } else {
+                timeouts.current.forEach(t => clearTimeout(t));
+                timeouts.current = []
+                wine.style.height = `10px`;
+            }
+        })
+    }
+
+    let observer = new IntersectionObserver(callback, options);
+    setTimeout(
+        () => {
+            observer.observe(document.querySelector('.' + styles.svgContainer));
+        }, 1000
+    );
+
+    const clickGlassHandler = () => {
+        ym('reachGoal','click-glass')
+        timeouts.current.forEach(t => clearTimeout(t));
+        timeouts.current = [];
+        const wine = document.querySelector('.' + styles.wine);
+        const curValue = Number(wine.style.height.replace('px', ''))
+        let timeout;
+        for (let i = 10; i < curValue; i++) {
+            timeout = setTimeout(
+                () => {
+                    wine.style.height = `${curValue - i + 10}px`;
+                }, (i - 10) * 10
+            )
+            timeouts.current.push(timeout);
+        }
+        for (let i = 10; i < 43; i++) {
+            timeout = setTimeout(
+                () => {
+                    wine.style.height = `${i}px`;
+                }, (curValue - 10) * 10 + (i - 10) * 100
+            )
+            timeouts.current.push(timeout);
+        }
+    }
 
 
     let decide, your, think;
@@ -41,7 +99,7 @@ const Details = ({ link }) => {
                     нашей семейной винотеки.</h3>
                 <div className={styles.svgContainer}>
                     <div style={{position: 'relative', overflow: 'hidden'}}>
-                        <img onClick={() => ym('reachGoal','click-glass')}  height='50px' src={glass} alt='glass' style={{zIndex: 1, position: 'relative'}} />
+                        <img onClick={clickGlassHandler}  height='50px' src={glass} alt='glass' style={{zIndex: 1, position: 'relative'}} />
                         <div className={styles.wine}></div>
                     </div>
                 </div>
